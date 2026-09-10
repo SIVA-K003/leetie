13eb3136b43f1c4372dc199148bd042ccc90f96a
SIVA-K003 <sivak003@users.noreply.github.com>
2026-09-10 05:43:39 +0000

    leetie: Add 621. Task Scheduler [Medium] (java)

---
+++ solutions/Medium/0621-task-scheduler/solution.java
@@ -0,0 +1,37 @@
+// ──────────────────────────────────────────────────
+// Problem  : 621. Task Scheduler
+// Difficulty: Medium
+// Tags     : Array, Hash Table, Greedy, Sorting, Heap (Priority Queue), Counting
+// Link     : https://leetcode.com/problems/task-scheduler/
+// Runtime  : 4 ms (beats 73%)
+// Memory   : 48264000 (beats 52%)
+// Language : java
+// Copyright: (c) 2026 SIVA-K003. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+public class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] counter = new int[26];
+        int max = 0;
+        int maxCount = 0;
+        for(char task : tasks) {
+            counter[task - 'A']++;
+            if(max == counter[task - 'A']) {
+                maxCount++;
+            }
+            else if(max < counter[task - 'A']) {
+                max = counter[task - 'A'];
+                maxCount = 1;
+            }
+        }
+        
+        int partCount = max - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.length - max * maxCount;
+        int idles = Math.max(0, emptySlots - availableTasks);
+        
+        return tasks.length + idles;
+    }
+}
