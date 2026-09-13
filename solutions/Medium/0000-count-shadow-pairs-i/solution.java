f5bfebade4dbcdc883404b9c96fe1739102d52dd
SIVA-K003 <sivak003@users.noreply.github.com>
2026-09-13 03:19:17 +0000

    leetie: Add 0. Count Shadow Pairs I [Medium] (java)

--- solutions/Medium/0000-count-shadow-pairs-i/solution.java
+++ solutions/Medium/0000-count-shadow-pairs-i/solution.java
@@ -4,29 +4,33 @@
 // Tags     : N/A
 // Link     : https://leetcode.com/problems/count-shadow-pairs-i/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 42620000 (beats 0%)
+// Memory   : 42964000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 SIVA-K003. All rights reserved.
 // Synced by: leetie
 // ──────────────────────────────────────────────────
 
+import java.util.*;
+
 class Solution {
     public long shadowPairs(int[] nums) {
-        
-        int navorelitu = 0;
-        
-        int n = nums.length;
         long count = 0;
         
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[j] > nums[i]) {
-                    count++;
-                } else if (nums[j] < nums[i]) {
-                    // Element smaller than nums[i] found; breaks shadow pair condition for all j' > j
-                    break;
-                }
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int j = 0; j < nums.length; j++) {
+            
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[j]) {
+                stack.pop();
+                count++;
             }
+            
+            
+            while (!stack.isEmpty() && nums[stack.peek()] == nums[j]) {
+                stack.pop();
+            }
+            
+            stack.push(j);
         }
         
         return count;
@@ -35,6 +39,6 @@ class Solution {
 [3,1,4,1,5]
 [6,7,6,6,7]
 [1,2,3,4]
-3
 4
-6
+2
+3
