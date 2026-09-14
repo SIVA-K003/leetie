2f0c8e37576bfcfd232f96ab80c691e574ac54bf
SIVA-K003 <sivak003@users.noreply.github.com>
2026-09-14 05:02:04 +0000

    leetie: Add 152. Maximum Product Subarray [Medium] (java)

--- solutions/Medium/0152-maximum-product-subarray/solution.java
+++ solutions/Medium/0152-maximum-product-subarray/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming
 // Link     : https://leetcode.com/problems/maximum-product-subarray/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42248000 (beats 0%)
+// Runtime  : 2 ms (beats 72%)
+// Memory   : 47384000 (beats 34%)
 // Language : java
 // Copyright: (c) 2026 SIVA-K003. All rights reserved.
 // Synced by: leetie
